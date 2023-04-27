61581015a59fdfb1baa5f594d7000335c14c6c02
MoccaHazel <wltn1937@gmail.com>
2023-04-27 14:59:41 +0900

    Add Boss Sample

---
+++ Assets/Script/Boss/EventSystemSample/EventListener.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EventListener : MonoBehaviour, IListener
+{
+    float sampleValue = 100;
+    
+    public float SampleValue
+    {
+        get { return sampleValue; }
+        set
+        {
+            sampleValue = value;
+            EventManager.instance.PostNotification(EVENT_TYPE.eHealthChange, this, SampleValue);
+        }
+    }
+    
+    void Start()
+    {
+        EventManager.instance.AddListener(EVENT_TYPE.eSampleEvent, this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            SampleValue -= 20;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            SampleValue -= 50;
+        }
+
+    }
+
+    public void OnEvent(EVENT_TYPE EventType, Component Sender, object Param = null)
+    {
+        switch(EventType)
+        {
+            case EVENT_TYPE.eSampleEvent:
+                Debug.Log("이벤트 실행. HP : "+ Param);
+                break;
+        }
+    }
+}
---
+++ Assets/Script/Boss/EventSystemSample/EventManager.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+
+    public static EventManager instance = null;
+    Dictionary<EVENT_TYPE, List<IListener>> Listeners = new();
+
+    //인스턴스화
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+            DestroyImmediate(gameObject);
+    }
+
+    //다른 곳에서 시작 시 호출하는 코드
+    public void AddListener(EVENT_TYPE eventType, IListener Listener)
+    {
+#pragma warning disable IDE0018 // 인라인 변수 선언
+#pragma warning disable IDE0059 // 불필요한 값 할당
+        List<IListener> ListenList = null;
+
+        if (Listeners.TryGetValue(eventType, out ListenList))
+        {
+            ListenList.Add(Listener);
+            return;
+        }
+
+#pragma warning disable IDE0028
+        ListenList = new List<IListener>();
+        ListenList.Add(Listener);
+        Listeners.Add(eventType, ListenList);
+    }
+
+
+    //특정 이벤트 발동 시 호출하는 코드
+    public void PostNotification(EVENT_TYPE eventType, Component Sender, object param= null)
+    {
+        List<IListener> ListenList = null;
+
+        if (!Listeners.TryGetValue(eventType, out ListenList))
+        {
+            return;
+        }
+            
+
+        for (int i = 0; i < ListenList.Count; i++)
+            ListenList?[i].OnEvent(eventType, Sender, param);
+    }
+
+
+    //오브젝트 파괴 시 리스트 관리용
+    public void RemoveRedundancies()
+    {
+        Dictionary<EVENT_TYPE, List<IListener>> newListeners = new();
+
+        foreach(KeyValuePair<EVENT_TYPE,List<IListener>> Item in Listeners)
+        {
+            for(int i = Item.Value.Count -1; i >= 0; i--)
+            {
+                if (Item.Value[i].Equals(null))
+                    Item.Value.RemoveAt(i);
+            }
+            
+            if (Item.Value.Count > 0)
+                newListeners.Add(Item.Key, Item.Value);
+        }
+
+        Listeners = newListeners;
+    }
+}
+
+//이벤트 이넘값
+public enum EVENT_TYPE  //
+{
+    eGameInit,
+    eGameEnd,
+    eAmmoCharge,
+    eHealthChange,
+    EDead,
+    bHit,
+    bFindPlayer,
+    bEnterBattle,
+    bGuarded,
+    bParried,
+    bHealthChange,
+    bDead,
+    eSampleEvent
+}
+public interface IListener
+{
+    void OnEvent(EVENT_TYPE EventType, Component Sender, object Param = null);
+}
+
+
