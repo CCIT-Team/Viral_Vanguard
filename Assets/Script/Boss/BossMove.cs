069473ec9952d85c1657260ba260940258edc150
JeonSeaStar <88014706+JeonSeaStar@users.noreply.github.com>
2023-04-28 10:30:13 +0900

    보스 애니메이터 수정

--- Assets/Script/Boss/BossMove.cs
+++ Assets/Script/Boss/BossMove.cs
@@ -7,12 +7,30 @@ public class BossMove : MonoBehaviour
 {
     [SerializeField] NavMeshAgent agent;
     [SerializeField] Transform target;
+    [SerializeField] Animator animator;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             agent.SetDestination(target.position);
+            animator.SetBool("Walk", true);
         }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            TargetTracking(agent.isStopped);
+        }
+
+        if (agent.velocity.sqrMagnitude <= 0 * 0.2f && agent.remainingDistance <= agent.stoppingDistance)
+        {
+            print("³¡!");
+        }
+    }
+
+    void TargetTracking(bool b)
+    {
+        agent.isStopped = b ? false : true;
+        animator.SetBool("Walk", b);
     }
 }
