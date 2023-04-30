2551ffc8a4276d34657d73ed6da98bc97f8a9547
JeonSeaStar <88014706+JeonSeaStar@users.noreply.github.com>
2023-05-01 00:09:20 +0900

    ui 예시

--- Assets/Script/Boss/BossUI.cs
+++ Assets/Script/Boss/BossUI.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class BossUi : MonoBehaviour
+public class BossUI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Image fillImage;
+    public float maxHP;
+    public float currentHP;
+
+    //public BossStatus boss1;
+    //public BossStatus boss1;
+    //public BossStatus boss1;
+    //public BossStatus boss1;
+    //public BossStatus boss1;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            //maxHP = boss1.maxHp;
+            //currentHP = boss1.currentHp;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            currentHP -= 10;
+            HpUpdate();
+        }
+    }
+
+    void HpUpdate()
+    {
+        fillImage.fillAmount = currentHP / maxHP;
     }
 }
+
+//public class BossStatus
+//{
+//    public int maxHp;
+//    public int currentHp;
+//    public string name;
+//}
