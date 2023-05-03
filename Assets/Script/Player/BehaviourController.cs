c312f06c402a39e2da2175c8e3d27c5fd8c13687
dy990320 <dy990320@naver.com>
2023-05-03 11:30:04 +0900

    플레이어 행동 컨트롤러 및 움직임 행동 그리고 카메라 보정 추가

---
+++ Assets/Script/Player/BehaviourController.cs
@@ -0,0 +1,261 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+/// <summary>
+/// 현재, 기본, 오버라이딩, 잠긴 동작들 + 마우스 이동값, 땅 확인, GenericBehaviour업데이트 시켜줌
+/// 플러거블 패턴
+/// </summary>
+[RequireComponent(typeof(CapsuleCollider)), RequireComponent(typeof(Rigidbody))]
+public class BehaviourController : MonoBehaviour
+{
+    private List<GenericBehaviour> behaviours;
+    private List<GenericBehaviour> overrideBehaviours;
+    private int currentBehaviours;
+    private int defaultBehaviour;
+    private int behaviourLocked;
+
+    public Transform playerCamera;
+    private Transform myTransform;
+    private Animator myAnimator;
+    private Rigidbody myRigidbody;
+    private LockOn camScript;
+
+    public float h;
+    public float v;
+    public float turnSmooth = 0.06f;
+    private bool changedFOV;
+    public float sprintFOV = 100;
+
+    private Vector3 lastDirection;
+    private bool sprint;
+    private int hFloat;
+    private int vFloat;
+    private int groundedBool;
+    private Vector3 colExtents;
+
+    public float GetH { get => h; }
+    public float GetV { get => v; }
+    public LockOn GetCamScript { get => camScript; }
+    public Rigidbody GetRigidbody { get => myRigidbody; }
+    public Animator GetAnimator { get => myAnimator; }
+    public int GetDefailtBehaviour { get => defaultBehaviour; }
+
+    private void Awake()
+    {
+        myTransform = transform;
+        behaviours = new List<GenericBehaviour>();
+        overrideBehaviours = new List<GenericBehaviour>();
+        myAnimator = GetComponent<Animator>();
+        hFloat = Animator.StringToHash(AnimatorKey.Horizontal);
+        vFloat = Animator.StringToHash(AnimatorKey.Vertical);
+        camScript = playerCamera.GetComponent<LockOn>();
+        myRigidbody = GetComponent<Rigidbody>();
+
+        groundedBool = Animator.StringToHash(AnimatorKey.Grounded);
+        colExtents = GetComponent<Collider>().bounds.extents;
+    }
+
+    public bool IsMoving()
+    {
+        return ((int)h != 0) || ((int)v != 0);
+    }
+
+    public bool IsHorizontalMoving()
+    {
+        return Mathf.Abs(h) > Mathf.Epsilon;
+    }
+
+    public bool IsGrounded()
+    {
+        Ray ray = new Ray(myTransform.position + Vector3.up * 2 * colExtents.x, Vector3.down);
+        return Physics.SphereCast(ray, colExtents.x, colExtents.x + 0.2f);
+    }
+
+    private void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        myAnimator.SetFloat(hFloat, h, 0.1f, Time.deltaTime);
+        myAnimator.SetFloat(vFloat, v, 0.1f, Time.deltaTime);
+
+        
+
+        myAnimator.SetBool(groundedBool, IsGrounded());
+    }
+
+    //낌 방지
+    public void Repositioning()
+    {
+        if(lastDirection != Vector3.zero)
+        {
+            lastDirection.y = 0;
+            Quaternion targetRotation = Quaternion.LookRotation(lastDirection);
+            Quaternion newRotation = Quaternion.Slerp(myRigidbody.rotation, targetRotation, turnSmooth);
+            myRigidbody.MoveRotation(newRotation);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        bool isAnyBehaniourActive = false;
+        if (behaviourLocked > 0 || overrideBehaviours.Count == 0)
+        {
+            foreach (GenericBehaviour behaviour in behaviours)
+            {
+                if(behaviour.isActiveAndEnabled && currentBehaviours == behaviour.GetBehaviourCode)
+                {
+                    isAnyBehaniourActive = true;
+                    behaviour.LocalFixedUpdate();
+                }
+            }
+        }
+        else
+        {
+            foreach (GenericBehaviour behaviour in overrideBehaviours)
+            {
+                behaviour.LocalFixedUpdate();
+            }
+        }
+
+        if(!isAnyBehaniourActive && overrideBehaviours.Count == 0)
+        {
+            myRigidbody.useGravity = true;
+            Repositioning();
+        }
+    }
+
+    //행동 등록
+    public void SubscribeBehaviour(GenericBehaviour behaviour)
+    {
+        behaviours.Add(behaviour);
+    }
+    //기본 행동 등록
+    public void RegisterDefaultBehavior(int behaviourCode)
+    {
+        defaultBehaviour = behaviourCode;
+        currentBehaviours = behaviourCode;
+    }
+
+    public void RegisterBehavior(int behaviourCode)
+    {
+        if (currentBehaviours == defaultBehaviour)
+        {
+            currentBehaviours = behaviourCode;
+        }
+    }
+    //행동 해제
+    public void UnRegisterBehavior(int behaviourCode)
+    {
+        if (currentBehaviours == behaviourCode)
+        {
+            currentBehaviours = defaultBehaviour;
+        }
+    }
+    //오버라이딩 행동 -- 우선시 되는 행동으로 덮어씌움
+    public bool OverrideWithBehaviour(GenericBehaviour behaviour)
+    {
+        if (!overrideBehaviours.Contains(behaviour))
+        {
+            if(overrideBehaviours.Count ==0)
+            {
+                foreach(GenericBehaviour behaviour1 in behaviours)
+                {
+                    if(behaviour1.isActiveAndEnabled && currentBehaviours == behaviour1.GetBehaviourCode)
+                    {
+                        behaviour1.OnOverride();
+                        break;
+                    }
+                }
+            }
+            overrideBehaviours.Add(behaviour);
+            return true;
+        }
+        return false;
+    }
+
+    public bool IsOverrideing(GenericBehaviour behaviour = null)
+    {
+        if(behaviour == null)
+        {
+            return overrideBehaviours.Count > 0;
+        }
+        return overrideBehaviours.Contains(behaviour);
+    }
+
+    public bool IsCurrentBehaviour(int behaviourCode)
+    {
+        return currentBehaviours == behaviourCode;
+    }
+
+    public bool GetTempLockStatus(int behaviourCode = 0)
+    {
+        return (behaviourLocked != 0 && behaviourLocked != behaviourCode);
+    }
+
+    public void LockTempBehaviour(int behaviourCode)
+    {
+        if(behaviourLocked == 0)
+        {
+            behaviourLocked = behaviourCode;
+        }
+    }
+
+    public void UnLockTempBehaviour(int behaviourCode)
+    {
+        if(behaviourLocked == behaviourCode)
+        {
+            behaviourLocked = 0;
+        }
+    }
+
+     public Vector3 GetLastDirection()
+    {
+        return lastDirection;
+    }
+
+    public void SetLastDirection(Vector3 direction)
+    {
+        lastDirection = direction;
+    }
+}
+
+
+public abstract class GenericBehaviour : MonoBehaviour
+{
+    protected int speedFloat;
+    protected BehaviourController behaviourController;
+    protected int behaviourCode;
+    protected bool canSpint;
+
+    private void Awake()
+    {
+        behaviourController = GetComponent<BehaviourController>();
+        speedFloat = Animator.StringToHash(AnimatorKey.Speed);
+        canSpint = true;
+
+        behaviourCode = GetType().GetHashCode();
+    }
+
+    public int GetBehaviourCode
+    {
+        get => behaviourCode;
+    }
+
+    public virtual void LocalLateUpdate()
+    {
+
+    }
+
+    public virtual void LocalFixedUpdate()
+    {
+
+    }
+
+    public virtual void OnOverride()
+    {
+
+    }
+}
--- Assets/Script/Player/PlayerCameraManager.cs
+++ Assets/Script/Player/PlayerCameraManager.cs
@@ -2,12 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class PlayerCameraManager : MonoBehaviour
 {
     [SerializeField]
-    public Camera cam;
+    public Camera myCamera;
 
     public Transform player;
+    public Vector3 pivotOffset = new Vector3(0.0f, 1.0f, 0.0f);
+    public Vector3 camOffset = new Vector3(0.0f, 2.0f, -4.0f);
+    public float smooth = 10f;
+    public float horizontalAimingSpeed;
+    public float verticalAimingSpeed;
+
+    //
+    public float maxVerticalAngle = 30.0f;
+    public float minVerticalAngle = -60.0f;
+    public float camShake = 4f;
+    private float angleH = 0.0f;
+    private float angleV = 0.0f;
+
+    //
+    private Transform cameraTransform;
+
+    //
+    private Vector3 relCameraPos;
+    private float relCameraPosMag;
+
+    private Vector3 smoothPivotOffset;
+    private Vector3 targetPivotOffset;
+    private Vector3 smoothCamOffset;
+    private Vector3 targetCamOffset;
+
+    private float defaultFOV; //기본 시야값
+    private float targetFOV; //타겟 시야값
+    private float targetMaxVerticleAngle; //카메라 수직 최대 각도
+    private float shakeAngle = 0f; 
+
+    public float GetH
+    {
+        get => angleH;
+    }
+
+    //
     public float smooth_speed = 0.125f;
     public Vector3 offset;
     bool inputMode = false;
@@ -19,12 +56,26 @@ public class PlayerCameraManager : MonoBehaviour
     public Sprite lockOnImage;
     private void Awake()
     {
-        if (lockOn == null) { GetComponent<LockOn>(); }
-        if (cam == null) { cam = Camera.main; }
-    }
-    void Start()
-    {
+        cameraTransform = transform;
+        myCamera = cameraTransform.GetComponent<Camera>();
 
+        //
+        cameraTransform.position = player.position + Quaternion.identity * pivotOffset + Quaternion.identity * camOffset;
+        cameraTransform.rotation = Quaternion.identity;
+
+        //
+        relCameraPos = cameraTransform.position - player.position;
+        relCameraPosMag = relCameraPos.magnitude - 0.5f;
+
+        smoothPivotOffset = pivotOffset;
+        smoothCamOffset = camOffset;
+        defaultFOV = myCamera.fieldOfView;
+        angleH = player.eulerAngles.y;
+        if (lockOn == null) { GetComponent<LockOn>(); }
+
+        ResetTargetOffsets();
+        ResetFOV();
+        ResetMaxVerticalAngle();
     }
 
     void FixedUpdate()
@@ -59,5 +110,74 @@ public class PlayerCameraManager : MonoBehaviour
             else { islockOn = true; }
             lockOn.Lock();
         }
+    }
+
+    public void ResetTargetOffsets()
+    {
+        targetPivotOffset = pivotOffset;
+        targetCamOffset = camOffset;
+    }
+
+    public void ResetFOV()
+    {
+        targetFOV = defaultFOV;
+    }
+
+    public void ResetMaxVerticalAngle()
+    {
+        targetMaxVerticleAngle = maxVerticalAngle;
+    }
+
+    public void ShakeVertical(float degree) 
+    {
+        shakeAngle = degree;
+    }
+
+    public void SetTargetOffset(Vector3 newPivotOffset, Vector3 newCamOffset) 
+    {
+        targetPivotOffset = newPivotOffset;
+        targetCamOffset = newCamOffset;
+    }
+
+    public void SetFOV(float customFOV) 
+    {
+        targetFOV = customFOV;
+    }
+
+    bool ViewingPosCheck(Vector3 checkPos, float playerHeight)
+    {
+        Vector3 target = player.position + (Vector3.up * playerHeight);
+        if(Physics.SphereCast(checkPos, 0.2f, target - checkPos, out RaycastHit hit, relCameraPosMag))
+        {
+            if(hit.transform != player && !hit.transform.GetComponent<Collider>().isTrigger)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool ReverseViewingPosCheck(Vector3 checkPos, float playerHeight, float maxDistance)
+    {
+        Vector3 origin = player.position + (Vector3.up * playerHeight);
+        if(Physics.SphereCast(origin, 0.2f, checkPos - origin, out RaycastHit hit, maxDistance))
+        {
+            if(hit.transform != player && hit.transform != transform && !hit.transform.GetComponent<Collider>().isTrigger)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool DoubleViewingCheck(Vector3 checkPos, float Offset)
+    {
+        float playerFocusHeight = player.GetComponent<CapsuleCollider>().height * 0.75f;
+        return ViewingPosCheck(checkPos, playerFocusHeight) && ReverseViewingPosCheck(checkPos, playerFocusHeight, Offset);
+    }
+
+    public float GetCurrentPivotMagnitude(Vector3 finalPivotOffset)
+    {
+        return Mathf.Abs((finalPivotOffset - smoothPivotOffset).magnitude);
     }
 }
