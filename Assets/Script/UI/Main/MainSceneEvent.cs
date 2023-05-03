81b40d215e7dfdc359d5fc9b240a0d1601d30f3f
Kang Seong Jun <dkdlfhqht789@naver.com>
2023-05-03 19:05:17 +0900

    Main Scene 구조 작업 버튼 이벤트 연결 Manager 생성 Canvas 계층도 정리

---
+++ Assets/Script/UI/Main/MainSceneEvent.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ButtonArrays
+{
+    // 저장된 데이터가 없는 경우
+    public GameObject[] noSaveDataVisualizationElements; 
+    // 저장된 데이터가 있는 경우
+    public GameObject[] SaveDataVisualizationElements;
+
+    public GameObject[] saveDataArrays;
+}
+
+public class MainSceneEvent : MonoBehaviour
+{
+    public bool isSaveDataExist = false;
+    public ButtonArrays buttonArrays = new ButtonArrays();
+
+    public void Awake()
+    {
+        if (isSaveDataExist)
+        {
+            for (int i = 0; i < buttonArrays.SaveDataVisualizationElements.Length; i++)
+                buttonArrays.SaveDataVisualizationElements[i].SetActive(true);
+        }
+        else
+        {
+            for (int i = 0; i < buttonArrays.noSaveDataVisualizationElements.Length; i++)
+                buttonArrays.noSaveDataVisualizationElements[i].SetActive(true);
+        }
+    }
+
+    public void Update()
+    {
+        
+    }
+}
