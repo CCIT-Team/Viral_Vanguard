f6482034d01db197a77da9fd69fc7b9d1c370c35
JeonSeaStar <88014706+JeonSeaStar@users.noreply.github.com>
2023-05-04 12:38:25 +0900

    보스 체력바 UI, 보스 공격

--- Assets/Script/Boss/BossUI.cs
+++ Assets/Script/Boss/BossUI.cs
@@ -6,53 +6,45 @@ using UnityEngine.UI;
 public class BossUI : MonoBehaviour
 {
     public Image fillImage;
-    public float maxHP;
-    public float currentHP;
 
     public BossStatus boss1;
     public BossStatus boss2;
     public BossStatus boss3;
 
+    public Text bossName;
+
     public BossStatus current;
-   
-    void Start()
-    {
-        
-    }
 
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Q))
         {
             current = boss1;
-            maxHP = current.maxHP;
-            currentHP = current.currentHP;
+            HpUpdate();
         }
 
         if (Input.GetKeyDown(KeyCode.W))
         {
             current = boss2;
-            maxHP = current.maxHP;
-            currentHP = current.currentHP;
+            HpUpdate();
         }
 
         if (Input.GetKeyDown(KeyCode.E))
         {
             current = boss3;
-            maxHP = current.maxHP;
-            currentHP = current.currentHP;
+            HpUpdate();
         }
 
         if (Input.GetKeyDown(KeyCode.S))
         {
-            currentHP -= 10;
-            current.currentHP = currentHP;
+            current.currentHP -= 10;
             HpUpdate();
         }
     }
 
     void HpUpdate()
     {
-        fillImage.fillAmount = currentHP / maxHP;
+        bossName.text = current.BossName;
+        fillImage.fillAmount = current.currentHP / current.maxHP;
     }
 }
