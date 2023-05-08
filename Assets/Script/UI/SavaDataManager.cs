1a531ce34e233f455e1e87bf2d890b1e5ba9c3b4
Kang Seong Jun <dkdlfhqht789@naver.com>
2023-05-08 19:08:59 +0900

    애셋 오류 해결 데이터 세이브 완료 로드 완료 Json 파일 생성 완료

--- Assets/Script/UI/SavaDataManager.cs
+++ Assets/Script/UI/SavaDataManager.cs
@@ -5,7 +5,7 @@ using System.IO;
 using UnityEngine;
 
 [System.Serializable]
-public class SaveDataValue
+public class SaveData
 {
     //Player
     public int stateHpUpCount = 0;
@@ -13,11 +13,12 @@ public class SaveDataValue
 
     //Weapon
     public float equipWeaponID = 0;
-    public Dictionary<string, bool> obtainWeaponList = new Dictionary<string, bool>();
+    public List<string> obtainWeaponList = new List<string>();
     
+
     //Boss
     public int bossCount = 5;
-    public Dictionary<string, bool> bossClearList = new Dictionary<string, bool>();
+    public List<string> bossClearList = new List<string>();
 
     //Time
     public float playTime = 0;
@@ -30,39 +31,71 @@ public class SaveDataValue
         equipWeaponID = 0;
         playTime = 0;
 
-        obtainWeaponList.Add("Weapon1", false);
-        obtainWeaponList.Add("Weapon2", false);
-        obtainWeaponList.Add("Weapon3", false);
-        obtainWeaponList.Add("Weapon4", false);
-        obtainWeaponList.Add("Weapon5", false);
+        obtainWeaponList.Add("Weapon1");
+        obtainWeaponList.Add("Weapon2");
+        obtainWeaponList.Add("Weapon3");
+        obtainWeaponList.Add("Weapon4");
+        obtainWeaponList.Add("Weapon5");
 
 
-        bossClearList.Add("Boss1", false); // ("보스 이름 , 클리어 여부) 
-        bossClearList.Add("Boss2", false); // ("보스 이름 , 클리어 여부) 
-        bossClearList.Add("Boss3", false); // ("보스 이름 , 클리어 여부) 
-        bossClearList.Add("Boss4", false); // ("보스 이름 , 클리어 여부) 
-        bossClearList.Add("Boss5", false); // ("보스 이름 , 클리어 여부) 
+        bossClearList.Add("Boss1"); // ("보스 이름 , 클리어 여부) 
+        bossClearList.Add("Boss2"); // ("보스 이름 , 클리어 여부) 
+        bossClearList.Add("Boss3"); // ("보스 이름 , 클리어 여부) 
+        bossClearList.Add("Boss4"); // ("보스 이름 , 클리어 여부) 
+        bossClearList.Add("Boss5"); // ("보스 이름 , 클리어 여부) 
     }
 }
 
 public class SavaDataManager : Singleton<SavaDataManager>
 {
-    public SaveDataValue saveDataValue = new SaveDataValue();
+    public SaveData saveData = new SaveData();
 
     private string SAVE_DATA_DIRECTORY;  // 저장할 폴더 경로
     private string SAVE_FILENAME = "/SaveFile.txt"; // 파일 이름
+
     public bool isSaveDataExist = true;
 
-    void GameSave()
+    
+
+    public void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+
+        SAVE_DATA_DIRECTORY = Application.dataPath + "/SaveData/";
+
+        saveData.stateHpUpCount = 20;
+        GameSave("/SaveFile.txt");
+        GameLoad();
+    }
+
+    public void GameDataUpdate()
     {
 
     }
 
-    void GameLoad()
+    public void GameSave(string SAVE_FILENAME)
     {
-        SAVE_DATA_DIRECTORY = Application.dataPath + "/SaveData/";
+        string json = JsonUtility.ToJson(saveData);
+        File.WriteAllText(SAVE_DATA_DIRECTORY + SAVE_FILENAME, json);
 
-        if (!Directory.Exists(SAVE_DATA_DIRECTORY))
+        Debug.Log(json);
+    }
+
+    public void GameLoad()
+    {
+        //if (!Directory.Exists(SAVE_DATA_DIRECTORY))
+        //{
+        //    isSaveDataExist = false;
+        //    Directory.CreateDirectory(SAVE_DATA_DIRECTORY);// SaveData 폴더 생성
+        //}
+
+        if(File.Exists(SAVE_DATA_DIRECTORY + SAVE_FILENAME)) // 파일 존재하는 상황
+        {
+            string loadJson = File.ReadAllText(SAVE_DATA_DIRECTORY + SAVE_FILENAME);
+            saveData = JsonUtility.FromJson<SaveData>(loadJson);
+            Debug.Log(saveData.stateHpUpCount);
+        }
+        else
         {
             isSaveDataExist = false;
             Directory.CreateDirectory(SAVE_DATA_DIRECTORY);// SaveData 폴더 생성
