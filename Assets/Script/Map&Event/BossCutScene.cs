49e1cd4e5321163838b7eeecba18afc4669784a7
tuost <fldh753@gmail.com>
2023-05-11 18:07:49 +0900

    컷씬 구상중

---
+++ Assets/Script/Map&Event/BossCutScene.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossCutScene : MonoBehaviour
+{
+    bool checkPlayer = false;
+    Transform playerPos;
+
+    public Animator bossAnimator;
+
+    void Start()
+    {
+        playerPos = null;
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void GetPlayerPosition(Transform player)
+    {
+        playerPos = player;
+    }
+
+    void PlayCutScene()
+    {
+        checkPlayer = true;
+        bossAnimator.SetTrigger("Play");
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            GetPlayerPosition(other.transform);
+            PlayCutScene();
+        }
+    }
+}
