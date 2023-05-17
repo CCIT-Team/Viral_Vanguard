fd0d1a52000aa90a50a8cca5f356f7d53617c4fe
tuost <fldh753@gmail.com>
2023-05-17 14:12:56 +0900

    맵 이벤트 추가

--- Assets/Script/Map&Event/Event1.cs
+++ Assets/Script/Map&Event/Event1.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Event1 : MonoBehaviour
 {
-    public Animation ani;
+    public Animator ani;
+    public Camera playerCam;
+    public GameObject event1;
 
     private void OnTriggerEnter(Collider other)
     {
-        ani.Play();
+        event1.SetActive(true);
+        ani.SetTrigger("Q");
+        playerCam.gameObject.SetActive(false);
     }
 }
