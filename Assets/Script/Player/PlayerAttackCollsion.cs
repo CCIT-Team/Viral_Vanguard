1b1fef6221cb50e921e4259370ffa90af404234c
dy990320 <dy990320@naver.com>
2023-05-22 23:37:44 +0900

    몬스터 경직 변경

--- Assets/Script/Player/PlayerAttackCollsion.cs
+++ Assets/Script/Player/PlayerAttackCollsion.cs
@@ -16,27 +16,40 @@ public class PlayerAttackCollsion : MonoBehaviour
             {
                 BossMove.instacne.currentHealthPoint -= damages[0];
                 behaviourController.kineticEnergy += increasekineticEnergy[0];
+
+                if (BossMove.instacne.canStiffen)
+                {
+                    BossMove.instacne.SetStiffen(0, BossMove.AttackDirection.RIGHT); //r = 0, l = 1, center = 2, bigbang = 3 // none, right, Left
+                }
             }
             else if(behaviourController.myAnimator.GetBool(AnimatorKey.Attack2)&& !behaviourController.myAnimator.GetBool(AnimatorKey.Attack3))
             {
                 BossMove.instacne.currentHealthPoint -= damages[1];
                 behaviourController.kineticEnergy += increasekineticEnergy[1];
 
+                if (BossMove.instacne.canStiffen)
+                { 
+                    BossMove.instacne.SetStiffen(1, BossMove.AttackDirection.LEFT); 
+                }
             }
             else if(behaviourController.myAnimator.GetBool(AnimatorKey.Attack3) && behaviourController.myAnimator.GetBool(AnimatorKey.Attack2))
             {
                 BossMove.instacne.currentHealthPoint -= damages[2];
                 behaviourController.kineticEnergy += increasekineticEnergy[2];
+
+                if (BossMove.instacne.canStiffen)
+                {
+                    BossMove.instacne.SetStiffen(2, BossMove.AttackDirection.NONE); 
+                }
             }
             else if(behaviourController.myAnimator.GetBool(AnimatorKey.BigBang))
             {
                 BossMove.instacne.currentHealthPoint -= damages[3];
-                //대경직 넣어야됨
-            }
-            if (BossMove.instacne.canStiffen)
-            {//경직 
-                print("sss");
-                BossMove.instacne.Stiffen = true;
+
+                if (BossMove.instacne.canStiffen)
+                {
+                    BossMove.instacne.SetStiffen(3, BossMove.AttackDirection.NONE); 
+                }
             }
         }
     }
