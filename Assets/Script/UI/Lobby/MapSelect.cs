b39b36272c7f0135c8006418c4480564701832bb
jojo <kkokk88@naver.com>
2023-05-24 02:47:32 +0900

    맵 선택 스크립트 업로드

--- Assets/Script/UI/Lobby/MapSelect.cs
+++ Assets/Script/UI/Lobby/MapSelect.cs
@@ -1,19 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class MapSelect : MonoBehaviour
+public class MapSelect : MonoBehaviour,IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
-    public GameObject section;
-    public Image sectionClick;
+    public GameObject sectionButton;
+    public MeshRenderer meshRenderer;
+    public Image selectCursorImage;
+    public Sprite selectCursorClickSprite;
+    public Sprite selectCursorOnSprite;
 
-    void CursorOnTheObjectroof()
+    public MapSelect[] mapSelects;
+
+    bool isSelected;
+
+    public void CursorOnTheObjectroof()
     {
-        sectionClick.material.color = new Color(0, 0, 75, 100);
+        meshRenderer.materials[0].SetColor("_EmissiveColor", new Color(0, 0, 38, 50));
+        meshRenderer.materials[1].SetColor("_EmissiveColor", new Color(0, 0, 75, 100));
     }
 
-    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        selectCursorImage.enabled = true; // 이미지 표시
+    }
 
-   
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (isSelected == false)
+            selectCursorImage.enabled = false; // 이미지 숨김
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        selectCursorImage.sprite = selectCursorClickSprite; // 클릭 이미지로 변경
+        isSelected = true;
+        CursorOnTheObjectroof();
+        OnOff();
+    }
+
+    void OnOff()
+    {
+        foreach (MapSelect mapSelect in mapSelects)
+        {
+            mapSelect.selectCursorImage.enabled = false;
+            mapSelect.selectCursorImage.sprite = mapSelect.selectCursorOnSprite;
+            mapSelect.meshRenderer.materials[0].SetColor("_EmissiveColor", new Color(18, 100, 65, 50));
+            mapSelect.meshRenderer.materials[1].SetColor("_EmissiveColor", new Color(30, 10, 75, 100));
+        }
+    }
 }
